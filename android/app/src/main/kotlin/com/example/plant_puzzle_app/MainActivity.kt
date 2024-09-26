1df609111e4eff25c7faf1b1df716ca282e18f2e
Sabina <sabina.aj@seznam.cz>
2024-09-26 16:16:46 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/plant_puzzle_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.plant_puzzle_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Seznam položek'),
+      ),
+      body: ItemList(),
+    );
+  }
+}
+
+class ItemList extends StatelessWidget {
+  final List<String> items = List<String>.generate(20, (i) => "Položka ${i + 1}");
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(items[index]),
+        );
+      },
+    );
+  }
+}
